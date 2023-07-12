9f33d374a345359c389829e57d194cc853d5a293
Mario Virdis <virdis.mario97@gmail.com>
2023-07-12 15:16:03 +0200

    Add Listening networking initialization

--- Source/UE_Simulator/UE_SimulatorGameMode.cpp
+++ Source/UE_Simulator/UE_SimulatorGameMode.cpp
@@ -1,4 +1,11 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+#pragma comment(lib, "Ws2_32.lib")
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
 #include "UE_SimulatorGameMode.h"
 #include "UE_SimulatorCharacter.h"
@@ -12,6 +19,10 @@ AUE_SimulatorGameMode::AUE_SimulatorGameMode()
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
+	// On construction networking is disabled
+	isNetworkingInitialized = false;
+	ServerPort = 22077;
+	ListenSocket = INVALID_SOCKET;
 }
 
 bool AUE_SimulatorGameMode::RegisterSensor(USensorCaptureComponent const* sensor) {
@@ -28,4 +39,103 @@ bool AUE_SimulatorGameMode::RegisterSensor(USensorCaptureComponent const* sensor
 		ActiveSensors.Add(sensor);
 		return true;
 	}
+}
+
+bool AUE_SimulatorGameMode::InitializeNetworking() {
+	if (isNetworkingInitialized) return false;
+	
+	UE_LOG(LogUESimulator, Log, TEXT("[AUE_SimulatorGameMode] Initializing Server Listening Socket..."));
+
+	// Make sure that networking is consistent if we exit early with error
+	isNetworkingInitialized = false;
+	ListenSocket = INVALID_SOCKET;
+
+	WSADATA wsaData;
+	int iResult;
+	u_long iMode = 1; // Non-blocking
+
+	// Initialize Winsock version 2
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		UE_LOG(LogUESimulator, Error, TEXT("[AUE_SimulatorGameMode] WSAStartup failed with error %d"), iResult);
+		return false;
+	}
+
+	// Setup address info
+	struct addrinfo* result = NULL, * ptr = NULL, hints;
+	FString PortString = FString::FromInt(ServerPort);
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM; // TCP transport protocol
+	hints.ai_protocol = IPPROTO_TCP; // TCP transport protocol
+	hints.ai_flags = AI_PASSIVE;
+
+	// Resolve the local address and port to be used by the server
+	iResult = getaddrinfo(NULL, (PCSTR) TCHAR_TO_ANSI(*PortString), &hints, &result);
+	if (iResult != 0) {
+		UE_LOG(LogUESimulator, Error, TEXT("[AUE_SimulatorGameMode] getaddrinfo failed with error %d"), iResult);
+		WSACleanup();
+		return false;
+	}
+
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		UE_LOG(LogUESimulator, Error, TEXT("[AUE_SimulatorGameMode] Error at socket() with error %ld"), WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return false;
+	}
+
+	// Set the mode of the socket to non-blocking
+	iResult = ioctlsocket(ListenSocket, FIONBIO, &iMode);
+	if (iResult != NO_ERROR) {
+		UE_LOG(LogUESimulator, Error, TEXT("[AUE_SimulatorGameMode] Failed to set in non-blocking mode with error %d"), WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		ListenSocket = INVALID_SOCKET;
+		WSACleanup();
+		return false;
+	}
+
+	// Setup the TCP listening socket
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		UE_LOG(LogUESimulator, Error, TEXT("[AUE_SimulatorGameMode] bind failed with error %d"), WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		ListenSocket = INVALID_SOCKET;
+		WSACleanup();
+		return false;
+	}
+
+	freeaddrinfo(result);
+
+	// Put the socket into listen mode (non-blocking)
+	iResult = listen(ListenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR) {
+		UE_LOG(LogUESimulator, Error, TEXT("[AUE_SimulatorGameMode] listen failed with error %d"), WSAGetLastError());
+		closesocket(ListenSocket);
+		ListenSocket = INVALID_SOCKET;
+		WSACleanup();
+		return false;
+	}
+
+	UE_LOG(LogUESimulator, Log, TEXT("[AUE_SimulatorGameMode] Successfully initialized server networking. Listening on port %d"), ServerPort);
+
+	return isNetworkingInitialized = true;
+}
+
+AUE_SimulatorGameMode::~AUE_SimulatorGameMode() {
+	UE_LOG(LogUESimulator, Log, TEXT("[AUE_SimulatorGameMode] Closing socket on destruction"));
+
+	// General Socket cleanup
+	closesocket(ListenSocket);
+	WSACleanup();
+}
+
+void AUE_SimulatorGameMode::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) {
+	Super::InitGame(MapName, Options, ErrorMessage);
+
+	InitializeNetworking();
 }
--- Source/UE_Simulator/UE_SimulatorGameMode.h
+++ Source/UE_Simulator/UE_SimulatorGameMode.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "CustomLogging.h"
 #include "GameFramework/GameModeBase.h"
+#include <winsock2.h>
 #include "UE_SimulatorGameMode.generated.h"
 
 UCLASS(ClassGroup = (Custom))
@@ -15,8 +16,25 @@ class AUE_SimulatorGameMode : public AGameModeBase
 	// List of active sensors in the level
 	TArray<class USensorCaptureComponent const *> ActiveSensors;
 
+	// ---- Windows Networking -----
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int ServerPort;
+
+private:
+	bool isNetworkingInitialized;
+
+	SOCKET ListenSocket;
+	// -----------------------------
+
 public:
 	AUE_SimulatorGameMode();
 
+	~AUE_SimulatorGameMode();
+
 	bool RegisterSensor(class USensorCaptureComponent const * sensor);
+
+	bool InitializeNetworking();
+
+	void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
 };
